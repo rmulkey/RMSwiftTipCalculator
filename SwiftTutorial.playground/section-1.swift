d64cbbaa27f4e0d3c12f8eb582335e5a065129d4
Rodrigo Mulkey <rmulkey@gmail.com>
2014-06-18 19:30:04 -0400

    playground fund =]

--- SwiftTutorial.playground/section-1.swift
+++ SwiftTutorial.playground/section-1.swift
@@ -2,8 +2,12 @@
 
 import UIKit
 
-var str = "Hello, playground"
 
+class PlaygroundFun {
+    
+}
+    
+var str = "Hello, playground"
 
 let tutorialTeam: Int = 56
 let editorialTeam = 23
@@ -11,10 +15,23 @@ var totalTeam = tutorialTeam + editorialTeam
 
 totalTeam += 1
 
-let princeInfered = 19.99
+let priceInferred = 19.99
 let priceExplicit: Double = 19.99
 
 let onSaleInferred = true
 let onSaleExplicit: Bool = false
 
-let nameInferred
+let nameInferred = "Rodrigo Mulkey"
+let nameExplicit: String = "Rodrigo Mulkey"
+
+var firstName = "Rodrigo"
+var lastName = "Mulkey"
+var fullName = firstName + lastName
+
+var fullNameFormatted = firstName + " " + lastName
+
+if onSaleInferred {
+    println("\(nameInferred) on sale for \(priceInferred)!")
+} else {
+    println("\(nameExplicit) on sale for \(priceExplicit)!")
+}
