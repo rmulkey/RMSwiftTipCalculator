edc95f5821a1cf5a06ecb53eb3f15bae57639581
Rodrigo Mulkey <rmulkey@gmail.com>
2014-06-18 19:21:32 -0400

    creating playground file, playing around with swift;

---
+++ SwiftTutorial.playground/section-1.swift
@@ -0,0 +1,20 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+let tutorialTeam: Int = 56
+let editorialTeam = 23
+var totalTeam = tutorialTeam + editorialTeam
+
+totalTeam += 1
+
+let princeInfered = 19.99
+let priceExplicit: Double = 19.99
+
+let onSaleInferred = true
+let onSaleExplicit: Bool = false
+
+let nameInferred
---
+++ TipCalculatorModel.swift
@@ -0,0 +1,9 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Rodrigo Mulkey on 6/18/14.
+//  Copyright (c) 2014 Rodrigo Mulkey. All rights reserved.
+//
+
+import Foundation
