e0316f812a681065908dae71dd5925b69e648c89
Rodrigo Mulkey <rmulkey@gmail.com>
2014-06-18 19:13:09 -0400

    adding basic project;

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Rodrigo Mulkey on 6/18/14.
+//  Copyright (c) 2014 Rodrigo Mulkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
